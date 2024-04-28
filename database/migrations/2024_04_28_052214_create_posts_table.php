c9ddc984227e3272a8626b87c4cf20ab9909c258
4th-avenue <quebec706@gmail.com>
2024-04-28 14:46:08 +0900

    Category와 Post 테이블에 컬럼들 추가하고 마이그레이션 실행 (Cascade)

--- database/migrations/2024_04_28_052214_create_posts_table.php
+++ database/migrations/2024_04_28_052214_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
