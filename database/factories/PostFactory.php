b1ccb093048984e529dcdeb92cb7529a6a996285
4th-avenue <quebec706@gmail.com>
2024-04-29 12:47:49 +0900

    테이블에 컬럼 추가하고 모델에 $fillable 정의 후 Seeder를 사용해 가상의 데이터 만들기

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,10 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(3),
+            'slug' => $this->faker->slug(),
+            'description' => $this->faker->paragraph(),
+            'category_id' => 1,
         ];
     }
 }
