7b9a36d95146f9d0fa71398b4f716a9b497062bc
4th-avenue <quebec706@gmail.com>
2024-04-30 19:50:02 +0900

    Categories와 Posts의 Index 페이지에 새 카테고리・새 글 작성 버튼 달기

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Categories') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Categories') }}
+            </h2>
+            <Link href="{{ route('categories.create') }}" class="px-4 py-2 bg-indigo-400 hover:bg-indigo-600 text-white text-sm font-medium rounded-md">
+                New Category
+            </Link>
+        </div>
     </x-slot>
 
     <div class="py-12">
