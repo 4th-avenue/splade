022f2c67dd1f406489f76cc232eb106d844d738e
4th-avenue <quebec706@gmail.com>
2024-04-30 11:44:22 +0900

    SPATIE Laravel-query-builder 설치 및 글로벌 검색 기능 강화

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,17 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                Collection::wrap($value)->each(function ($value) use ($query) {
+                    $query
+                        ->orWhere('title', 'LIKE', "%{$value}%")
+                        ->orWhere('slug', 'LIKE', "%{$value}%");
+                });
+            });
+        });
+
+        $posts = QueryBuilder::for(Post::class)
+        ->defaultSort('title')
+        ->allowedSorts(['title', 'slug'])
+        ->allowedFilters(['title', 'slug', $globalSearch]);
+
         return view('posts.index', [
-            'posts' => SpladeTable::for(Post::class)
+            'posts' => SpladeTable::for($posts)
                 ->column('title', canBeHidden: false, sortable: true)
                 ->withGlobalSearch(columns: ['title'])
-                ->column('slug')
+                ->column('slug', sortable: true)
                 ->paginate(5),
         ]);
     }
